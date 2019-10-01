c0621827a53c2bcd1eede2001f570b7b9c3dfa9e
Uglykoyote <uglykoyote@yandex.ru>
2019-10-01 10:13:48 +0400

    Update get_file.py

--- get_file.py
+++ get_file.py
@@ -10,8 +10,8 @@ minioClient = Minio('172.0.5.101:9002',
                   http_client=None)
 
 try:
-    data = minioClient.get_partial_object('forpic','test1.jpg',offset=1000)
-    with open('./test.jpg', 'wb') as file_data:
+    data = minioClient.get_partial_object('forpic','intest.jpg',offset=1000)
+    with open('./outtest.jpg', 'wb') as file_data:
         for d in data:
             file_data.write(d)
 except ResponseError as err:
